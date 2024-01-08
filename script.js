04004951681a81f5788e90a7c2d7129057f39986
Md. Waliullah Siam <110424153+waliullahsiam@users.noreply.github.com>
2024-01-08 17:47:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -15,20 +15,21 @@ inpAge.addEventListener("input", () => {
 
 
   if (inpAge.value === "") {
-        card1.style.visibility = "visible";
-        card2.style.visibility = "visible";
+        card1.style.display = "block";
+        card2.style.display = "block";
     } else {
         let age = parseInt(inpAge.value);
 
         if (age >= 18) {
-            card2.style.visibility = "visible";
-            card1.style.visibility = "hidden";
+            card2.style.display = "block";
+            card1.style.display = "none";
         } else {
-            card2.style.visibility = "hidden";
-            card1.style.visibility = "visible";
+            card2.style.display = "none";
+            card1.style.display = "block";
         }
 
     }
+    
 
 
 
